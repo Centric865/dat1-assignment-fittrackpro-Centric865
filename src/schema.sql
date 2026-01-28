07ca24067ba18f2c37d93b263e971e7f3c5ec9d4
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-01-28 15:36:31 +0000

    1 version 1 schema complete

--- src/schema.sql
+++ src/schema.sql
@@ -1,2 +1,20 @@
 .open fittrackpro.db
 .mode column
+
+CREATE TABLE locations(
+    location_id INTEGER PRIMARY KEY,
+    name TEXT,
+    address VARCHAR(7),
+    phone_number VARCHAR(11),
+    email VARCHAR CHECK (email LIKE '%@%'),
+    opening_hours TIME
+);
+
+CREATE TABLE members(
+    member_id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    email VARCHAR CHECK (email LIKE '%@%')
+    phone_number INTEGER,
+    date_of_birth DATE 
+)
