65669d75049f99efb1db9bb96659340af63a2dfb
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-01-30 13:05:03 +0000

    queries from task 1 done

--- src/queries/1_user_management.sql
+++ src/queries/1_user_management.sql
@@ -6,16 +6,48 @@ SELECT member_id, first_name, last_name, email, join_date
 FROM members;
 
 -- 1.2
+UPDATE members
+SET phone_number = '07000 100005', email = 'emily.jones.updated@email.com'
+WHERE member_id = 5;
 
-
+SELECT email
+FROM members 
+WHERE member_id = 5;
 -- 1.3
-
+SELECT COUNT(*) AS total_members
+FROM Members;
 
 -- 1.4
-
-
+SELECT 
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(*) AS registration_count
+FROM class_attendance ca
+JOIN members m
+    ON ca.member_id = m.member_id
+GROUP BY m.member_id
+ORDER BY registration_count DESC
+LIMIT 1; 
 -- 1.5
 
+SELECT 
+    m.member_id,
+    m.first_name,
+    m.last_name,
+    COUNT(*) AS registration_count
+FROM class_attendance ca
+JOIN members m
+    ON ca.member_id = m.member_id
+GROUP BY m.member_id
+ORDER BY registration_count ASC
+LIMIT 1;
 
 -- 1.6
-
+SELECT COUNT(*) AS member_count
+FROM    (
+    SELECT member_id
+    FROM class_attendance
+    GROUP BY member_id
+    HAVING COUNT(*) >= 2
+);
