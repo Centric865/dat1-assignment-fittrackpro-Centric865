4510cad9413e3aa18156a0d79ba2962aca3f4e52
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-02-10 15:55:27 +0000

    4.1 done

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -2,6 +2,12 @@
 .mode column
 
 -- 4.1 
+SELECT c.class_id, 
+       c.name AS class_name, 
+       printf('%s% %%s', s.first_name, s.last_name) AS instructor_name
+FROM classes c
+INNER JOIN class_schedule cs ON c.class_id = cs.class_id
+INNER JOIN staff s ON cs.staff_id = s.staff_id;
 
 
 -- 4.2 
