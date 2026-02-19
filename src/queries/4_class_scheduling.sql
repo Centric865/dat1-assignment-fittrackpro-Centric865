1a2acc2d6811ccc5e00368bfce8bd77f7cf2c5a8
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-02-19 13:43:57 +0000

    4.2 done need to check again

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -11,7 +11,16 @@ INNER JOIN staff s ON cs.staff_id = s.staff_id;
 
 
 -- 4.2 
-
+SELECT c.class_id, 
+       c.name, 
+       cs.start_time, 
+       cs.end_time, 
+       (c.capacity - COUNT(ca.member_id)) AS available_spots
+FROM classes c
+INNER JOIN class_schedule cs ON c.class_id = cs.class_id
+LEFT JOIN class_attendance ca ON cs.schedule_id = ca.schedule_id
+WHERE DATE(cs.start_time) = '2025-02-01' 
+GROUP BY c.class_id, c.name, cs.start_time, cs.end_time, c.capacity;
 
 -- 4.3 
 
