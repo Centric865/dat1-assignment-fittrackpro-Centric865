f343a610d63cbe6cb2bf50de59fa9f24b34298c4
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-02-19 22:34:48 +0000

    changes to 4

--- src/queries/4_class_scheduling.sql
+++ src/queries/4_class_scheduling.sql
@@ -2,20 +2,14 @@
 .mode column
 
 -- 4.1 
-SELECT c.class_id, 
-       c.name AS class_name, 
-       printf('%s% %%s', s.first_name, s.last_name) AS instructor_name
+SELECT c.class_id,c.name AS class_name, printf('%s% %%s', s.first_name, s.last_name) AS instructor_name
 FROM classes c
 INNER JOIN class_schedule cs ON c.class_id = cs.class_id
 INNER JOIN staff s ON cs.staff_id = s.staff_id;
 
 
 -- 4.2 
-SELECT c.class_id, 
-       c.name, 
-       cs.start_time, 
-       cs.end_time, 
-       (c.capacity - COUNT(ca.member_id)) AS available_spots
+SELECT c.class_id, c.name, cs.start_time, cs.end_time, (c.capacity - COUNT(ca.member_id)) AS available_spots
 FROM classes c
 INNER JOIN class_schedule cs ON c.class_id = cs.class_id
 LEFT JOIN class_attendance ca ON cs.schedule_id = ca.schedule_id
