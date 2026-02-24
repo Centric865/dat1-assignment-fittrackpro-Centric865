9c39d6b85ee225a64a4ac4dfbec37f6bf146933c
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-02-24 19:50:23 +0000

    7 done

--- src/queries/7_staff_management.sql
+++ src/queries/7_staff_management.sql
@@ -8,4 +8,9 @@ ORDER BY position;
 
 
 -- 7.2 
-
+SELECT s.staff_id, s.first_name, s.last_name, COUNT(pts.session_id) AS session_count
+FROM staff s
+JOIN personal_training_sessions pts ON s.staff_id = pts.staff_id
+WHERE pts.session_date >= '2025-01-20' 
+  AND pts.session_date <= DATE('2025-01-20', '+30 days')
+GROUP BY s.staff_id, s.first_name, s.last_name;
--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -2,4 +2,9 @@
 .mode column
 
 -- 8.1 
-
+SELECT pts.session_id, m.first_name, m.last_name, pts.session_date, pts.start_time, pts.end_time
+FROM personal_training_sessions pts
+JOIN staff s ON pts.staff_id = s.staff_id
+JOIN members m ON pts.member_id = m.member_id
+WHERE s.first_name = 'Ivy' 
+  AND s.last_name = 'Irwin';
