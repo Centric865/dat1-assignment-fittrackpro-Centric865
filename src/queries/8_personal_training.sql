75337121ab868e802e494254c6f8c4631db1a524
Rezwan68 <rezwanur.rahman@ada.ac.uk>
2026-02-27 15:28:23 +0000

    2

--- src/queries/8_personal_training.sql
+++ src/queries/8_personal_training.sql
@@ -6,5 +6,4 @@ SELECT pts.session_id, printf('%s %s', m.first_name, m.last_name) AS member_name
 FROM personal_training_sessions pts
 JOIN staff s ON pts.staff_id = s.staff_id
 JOIN members m ON pts.member_id = m.member_id
-WHERE s.first_name = 'Ivy' 
-  AND s.last_name = 'Irwin';
+WHERE s.first_name = 'Ivy' AND s.last_name = 'Irwin';
